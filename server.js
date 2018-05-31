5f26bb504920bc6e57d4f3564887616bc229e27c
Mohd Zaid <mzaidgaur@gmail.com>
2018-05-31 20:46:31 +0530

    Setup heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -54,6 +55,6 @@ app.get('/bad',(req,res)=>{
 	});
 });
 
-app.listen(3000,()=>{
-	console.log('Up');
+app.listen(port,()=>{
+	console.log(`Up at ${port}`);
 });
