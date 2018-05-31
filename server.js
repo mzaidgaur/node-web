f48b38a67118817a88fd036d84baeebe4df3d13d
Mohd Zaid <mzaidgaur@gmail.com>
2018-05-31 20:55:06 +0530

    Adding Project

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about',(req,res)=>{
 	});
 });
 
+app.get('/projects',(req,res)=>{
+	res.render('projects.hbs',{
+		title:'Projects'
+	});
+});
+
 app.get('/bad',(req,res)=>{
 	//res.send('<h1>Hi</h1>');
 	res.send({
